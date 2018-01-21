ead1b344ad39ed6f1be02fff1cb8e7a2d2e0a9b3
Rick Battipede <rbattipede@gmail.com>
2018-01-21 20:44:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(obj, artist, title){
 }
 
 function removeFromPlaylist(obj, artist){
-  delete playlist.artist
-  return playlist;
+  delete playlist.${artist}
+  return obj;
 }
